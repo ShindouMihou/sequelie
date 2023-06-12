3a6051646ba845168b06943f738e42098192b478
Shindou Mihou <git@mihou.pw>
2023-06-12 23:04:00 +0800

    feat(api): add `WithSettings` functions

--- sequelie.go
+++ sequelie.go
@@ -12,12 +12,24 @@ func ReadFile(file string) error {
 	return readFile(file, &Settings)
 }
 
+func ReadFileWithSettings(file string, settings *Options) error {
+	return readFile(file, settings)
+}
+
 func ReadDirectories(directories ...string) error {
 	return readDirs(directories, &Settings)
 }
 
+func ReadDirectoriesWithSettings(settings *Options, directories ...string) error {
+	return readDirs(directories, settings)
+}
+
 func ReadDirectory(dir string) error {
 	return readDir(dir, &Settings)
+}
+
+func ReadDirectoryWithSettings(dir string, settings *Options) error {
+	return readDir(dir, settings)
 }
 
 func GetAndTransform(address string, transformers Map) string {
